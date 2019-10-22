68f30303a0163f2aeddfc9b837050c8fd70d4ad0
joannvuong <vuongjoann@gmail.com>
2019-10-21 19:10:38 -0600

    added error handling to script.py

--- script.py
+++ script.py
@@ -1,1 +1,10 @@
-print('hello world')
+import json
+
+with open('data/example/example.json') as f:
+    jsondata = json.load(f)
+    options = jsondata['options']
+    parser = options['parser']
+    if 'decodeEntities' in parser:
+        print(parser['decodeEntities'])
+    else:
+        print('parser did not work')
