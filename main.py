3d481dc82f614eda2aac458816b7833df00d2cc9
Zach Wilson <zachwilson130+EcZachly@gmail.com>
2020-05-15 12:28:31 -0700

    configuring spaces

--- main.py
+++ main.py
@@ -2,8 +2,10 @@ from combiner import Combiner
 from analyzer import Analyzer
 from combiner_config import MessagesConfig, PostsConfig, LocationsConfig, LikesConfig, CommentsConfig
 from analyzer_config import MessagesAnalyzerConfig, CommentsAnalyzerConfig, PostsAnalyzerConfig
+
 combiner = Combiner(config=PostsConfig)
 files = combiner.combine()
+
 analyzer = Analyzer(config=PostsAnalyzerConfig)
 analyzer.analyze()
 
